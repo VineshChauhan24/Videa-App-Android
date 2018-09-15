5a0667c4052ac8fc7d7dee522f6a7301eb0e3127
Maik Luiz Paixão <paixomaik100@outlook.com.br>
2018-09-14 23:14:29 -0300

    ActivityHome added

---
+++ app/src/main/java/sokko/com/br/videaapp/Ideia.java
@@ -0,0 +1,26 @@
+package sokko.com.br.videaapp;
+
+/**
+ * Created by paixo on 14/09/2018.
+ */
+
+public class Ideia {
+    private String title;
+    private String description;
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+}
--- app/src/main/java/sokko/com/br/videaapp/LoginActivity.java
+++ app/src/main/java/sokko/com/br/videaapp/LoginActivity.java
@@ -1,19 +1,32 @@
 package sokko.com.br.videaapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText ed1;
     EditText ed2;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ed2 = findViewById(R.id.loginText2);
         ed1 = findViewById(R.id.loginText);
+        btn = findViewById(R.id.btnLogin);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+   public void onClick(View view){
+       startActivity(new Intent("sokko.com.br.videaapp.HomeActivity"));
+   }
+
 }
